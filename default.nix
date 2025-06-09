b3f59f730f6d9b765c25ab90a6983e31a03e5a67
Ahmed ElSayed <ahmed@elsayed.io>
2025-06-09 01:38:12 -0700

    flip packages merge order

--- default.nix
+++ default.nix
@@ -61,4 +61,4 @@
 in
   # We want the packages but also add a "default" that just points to the
   # latest released version.
-  taggedPackages // masterPackages // {"default" = taggedPackages.${latest};}
+  taggedPackages // {"default" = taggedPackages.${latest};}
